645c3a607ccc319f38a9a5254da368e152ea5f54
Bu4ra-3mer <boshraamer30@gmail.com>
2026-01-13 02:22:25 +0200

    edit mode

--- lib/models/tune_model.dart
+++ lib/models/tune_model.dart
@@ -4,9 +4,16 @@ import 'package:flutter/material.dart';
 class TuneModel {
   final Color color;
   final String sound;
-  const TuneModel({required this.color, required this.sound});
-  void playSound() {
-    final player = AudioPlayer();
-    player.play(AssetSource(sound));
+
+  const TuneModel({
+    required this.color,
+    required this.sound,
+  });
+
+  static final AudioPlayer _player = AudioPlayer();
+
+  void playSound() async {
+    await _player.stop();
+    await _player.play(AssetSource(sound));
   }
 }
